bb97a37fad5b95edc96721e7e9ab8b02f2559a66
yinonov <yinon@hotmail.com>
2020-03-28 23:16:34 +0300

    pwa & firestore ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/firebase.ts
@@ -0,0 +1,10 @@
+export const firebase = {
+  apiKey: 'AIzaSyDDdA9HiFEBsGGxcG1xfbJHKEr7LkziaQM',
+  authDomain: 'joie-app.firebaseapp.com',
+  databaseURL: 'https://joie-app.firebaseio.com',
+  projectId: 'joie-app',
+  storageBucket: 'joie-app.appspot.com',
+  messagingSenderId: '999974863094',
+  appId: '1:999974863094:web:37ae6deb807f3a8d206cd7',
+  measurementId: 'G-4L534VZ3CD'
+};
