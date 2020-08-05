9011978f7a12fd53bf0a7ef90bbd35a92f9958c6
danyrtmn <dany.rtmn@gmail.com>
2020-08-05 18:25:15 +0300

    separating header and side-nav from teacher template

--- apps/joie/src/app/teacher/teacher.module.ts
+++ apps/joie/src/app/teacher/teacher.module.ts
@@ -4,9 +4,11 @@ import { MatListModule } from '@angular/material/list';
 import { SharedModule } from '../shared/shared.module';
 import { TeacherRoutingModule } from './teacher-routing.module';
 import { TeacherComponent } from './container/teacher/teacher.component';
+import { SideNavComponent } from './container/side-nav/side-nav.component';
+import { HeaderComponent } from './container/header/header.component';
 
 @NgModule({
-  declarations: [TeacherComponent],
+  declarations: [TeacherComponent, SideNavComponent, HeaderComponent],
   imports: [SharedModule, TeacherRoutingModule, MatListModule],
 })
 export class TeacherModule {}
