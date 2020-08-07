6a27f03898e20c5efb84878ad83293b724776234
yinonov <yinon@hotmail.com>
2020-08-07 12:36:52 +0300

    contact page with contact form POC #13

---
+++ apps/joie/src/app/contact/containers/contact/contact-options.ts
@@ -0,0 +1,25 @@
+export const contactOptions = [
+  {
+    imgUrl: '/assets/images/screen.svg',
+    title: 'Visit our FAQ',
+    description: 'We might have answered it before.',
+  },
+  {
+    title: 'Send an Email',
+    description: 'Submit our contact form. Let us know how we may assist you.',
+  },
+  {
+    title: 'Join Our Family',
+    description: 'Follow us, share a funny meme or a lol.',
+  },
+  {
+    title: 'Give Us a Call',
+    description:
+      'We will be happy to hear from you, and promise to be friendly.',
+  },
+  {
+    title: 'Send a Postcard',
+    description:
+      '(or a gift card for the holidays). We will send you one back.',
+  },
+];
--- apps/joie/src/app/contact/containers/contact/contact.component.ts
+++ apps/joie/src/app/contact/containers/contact/contact.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { contactOptions } from './contact-options';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  contactOptions = contactOptions;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
