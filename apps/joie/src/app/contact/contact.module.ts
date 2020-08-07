5f8bcceaca31f5ca94d6d8a32967527a5b83e078
yinonov <yinon@hotmail.com>
2020-08-07 19:15:44 +0300

    contact page - all sections

--- apps/joie/src/app/contact/contact.module.ts
+++ apps/joie/src/app/contact/contact.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './containers/contact/contact.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { ContactService } from './services/contact.service';
-
+import { ContactMapComponent } from './components/contact-map/contact-map.component';
 
 @NgModule({
-  declarations: [ContactComponent, ContactFormComponent],
-  imports: [
-    SharedModule,
-    ContactRoutingModule
+  declarations: [
+    ContactComponent,
+    ContactFormComponent,
+    ContactMapComponent,
   ],
-  providers: [
-    ContactService
-  ]
+  imports: [SharedModule, ContactRoutingModule, GoogleMapsModule],
+  providers: [ContactService],
 })
-export class ContactModule { }
+export class ContactModule {}
