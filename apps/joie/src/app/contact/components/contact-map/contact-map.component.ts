680b5ae4eb4cc386b139644532697022e240bd83
yinonov <yinon@hotmail.com>
2020-08-08 17:37:14 +0300

    google map modifications

--- apps/joie/src/app/contact/components/contact-map/contact-map.component.ts
+++ apps/joie/src/app/contact/components/contact-map/contact-map.component.ts
@@ -1,37 +1,52 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps';
-
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-contact-map',
   templateUrl: './contact-map.component.html',
   styleUrls: ['./contact-map.component.scss'],
 })
-export class ContactMapComponent implements OnInit {
+export class ContactMapComponent {
   @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
   @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow;
 
-  zoom = 12;
-  center: google.maps.LatLngLiteral;
-  options: google.maps.MapOptions = {
-    zoomControl: false,
-    scrollwheel: false,
-    disableDoubleClickZoom: true,
-    // mapTypeId: 'hybrid',
-    maxZoom: 15,
-    minZoom: 8,
+  zoom = 17;
+  center: google.maps.LatLngLiteral = {
+    lat: 43.6449299,
+    lng: -79.3943352,
   };
-  markers = [];
+  options: google.maps.MapOptions = {
+    // zoomControl: false,
+    // scrollwheel: false,
+    // disableDoubleClickZoom: true,
+    // mapTypeId: 'hybrid',
+    // maxZoom: 15,
+    // minZoom: 8,
+  };
+  markers = [
+    {
+      position: this.center,
+      label: {
+        color: 'red',
+        text: 'Marker label ',
+      },
+      title: 'Marker title ',
+      info: 'Marker info ',
+      // options: {
+      //   animation: google.maps.Animation.BOUNCE,
+      // },
+    },
+  ];
   infoContent = '';
 
-  ngOnInit() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.center = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
-    });
-  }
+  // ngOnInit() {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     this.center = {
+  //       lat: position.coords.latitude,
+  //       lng: position.coords.longitude,
+  //     };
+  //   });
+  // }
 
   zoomIn() {
     if (this.zoom < this.options.maxZoom) this.zoom++;
@@ -41,31 +56,31 @@ export class ContactMapComponent implements OnInit {
     if (this.zoom > this.options.minZoom) this.zoom--;
   }
 
-  click(event: google.maps.MouseEvent) {
-    console.log(event);
-  }
+  // click(event: google.maps.MouseEvent) {
+  //   console.log(event);
+  // }
 
-  logCenter() {
-    console.log(JSON.stringify(this.map.getCenter()));
-  }
+  // logCenter() {
+  //   console.log(JSON.stringify(this.map.getCenter()));
+  // }
 
-  addMarker() {
-    this.markers.push({
-      position: {
-        lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
-        lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
-      },
-      label: {
-        color: 'red',
-        text: 'Marker label ' + (this.markers.length + 1),
-      },
-      title: 'Marker title ' + (this.markers.length + 1),
-      info: 'Marker info ' + (this.markers.length + 1),
-      options: {
-        animation: google.maps.Animation.BOUNCE,
-      },
-    });
-  }
+  // addMarker() {
+  //   this.markers.push({
+  //     position: {
+  //       lat: this.center.lat + ((Math.random() - 0.5) * 2) / 10,
+  //       lng: this.center.lng + ((Math.random() - 0.5) * 2) / 10,
+  //     },
+  //     label: {
+  //       color: 'red',
+  //       text: 'Marker label ' + (this.markers.length + 1),
+  //     },
+  //     title: 'Marker title ' + (this.markers.length + 1),
+  //     info: 'Marker info ' + (this.markers.length + 1),
+  //     options: {
+  //       animation: google.maps.Animation.BOUNCE,
+  //     },
+  //   });
+  // }
 
   openInfo(marker: MapMarker, content) {
     this.infoContent = content;
