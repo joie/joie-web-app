43e2531fae89066439bd0f16d99f034367659b32
danyrtmn <dany.rtmn@gmail.com>
2020-08-23 11:34:01 +0300

    attributes fix

--- apps/joie/src/app/session-form/components/session-form-attributes/session-form-attributes.component.ts
+++ apps/joie/src/app/session-form/components/session-form-attributes/session-form-attributes.component.ts
@@ -1,22 +1,8 @@
 import { Component } from '@angular/core';
 import { Pillar, CourseLevel, Activities } from '../../../sessions/models/session';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormControl, FormArray } from '@angular/forms';
 import { SessionFormService } from '../../services/session-form.service';
 import { SessionFormExtenderComponent } from '../../common/session-form-extender/session-form-extender.component';
-
-const newGoal = () =>
-  new FormGroup({
-    date: new FormControl(null),
-    time: new FormControl(null),
-    duration: new FormControl(null),
-  });
-
-const newComment = () =>
-  new FormGroup({
-    date: new FormControl(null),
-    time: new FormControl(null),
-    duration: new FormControl(null),
-  });
 
 @Component({
   selector: 'app-session-form-attributes',
@@ -37,8 +23,8 @@ export class SessionFormAttributesComponent extends SessionFormExtenderComponent
       ['pillar', new FormControl(null)],
       ['level', new FormControl(null)],
       ['activity', new FormControl(null)],
-      ['goals', new FormArray([new FormControl(null)])],
-      ['comments', new FormArray([new FormControl(null)])],
+      ['goals', this.goalsFormArray],
+      ['comments', this.commentsFormArray],
     ];
   }
 
