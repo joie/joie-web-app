0f965beaedb41cdade338435d4eb5d2b79e5db1b
danyrtmn <dany.rtmn@gmail.com>
2020-08-26 21:27:39 +0300

    goals step: goals->pillars, data structure refactoring

--- apps/joie/src/app/onboarding/onboarding-student/components/goal-step/goal-step.component.ts
+++ apps/joie/src/app/onboarding/onboarding-student/components/goal-step/goal-step.component.ts
@@ -1,3 +1,4 @@
+import { Pillar } from './../../../../sessions/models/session';
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 import { StudentOnboardingService } from '../../service/student-onboarding.service';
@@ -11,54 +12,78 @@ import { Router, RouterState } from '@angular/router';
 })
 export class GoalStepComponent implements OnInit {
   formGroup: FormGroup;
-  goalsData = [
-    { goal: 'Joie Movement', isChecked: false },
-    { goal: 'Joie Emotions', isChecked: false },
-    { goal: 'Joie Connections', isChecked: false },
-    { goal: 'Joie Spirit', isChecked: false },
-    { goal: 'Joie Professional', isChecked: false },
-  ];
-  cachedFormData;
+  pillarEnum = Pillar;
+
+  get pillarKeys() {
+    return Object.keys(Pillar);
+  }
+
+  get pillarFormArray() {
+    return this.formGroup.controls.pillars as FormArray;
+  }
+  // goalsData = [
+  //   { goal: 'Joie Movement', isChecked: false },
+  //   { goal: 'Joie Emotions', isChecked: false },
+  //   { goal: 'Joie Connections', isChecked: false },
+  //   { goal: 'Joie Spirit', isChecked: false },
+  //   { goal: 'Joie Professional', isChecked: false },
+  // ];
+  // cachedFormData;
   constructor(
     private _formBuilder: FormBuilder,
     public onboardingService: StudentOnboardingService,
     private router: Router
   ) {
     this.formGroup = this._formBuilder.group({
-      goalsCtrl: new FormArray([], atLeastOneIsCheckedValidator()),
+      // goalsCtrl: new FormArray([], atLeastOneIsCheckedValidator()),
+      pillars: new FormArray([]),
     });
+    this.addPillarCheckboxes();
+  }
+
+  private addPillarCheckboxes() {
+    this.pillarKeys.forEach(() => this.pillarFormArray.push(new FormControl(false)));
+    console.log(this.pillarFormArray);
+  }
+
+  submit() {
+    const selectedPillarTitles = this.formGroup.value.pillars
+      .map((checked, i) => (checked ? this.pillarEnum[this.pillarKeys[i]] : null))
+      .filter((v) => v !== null);
+
+    return selectedPillarTitles;
   }
 
   ngOnInit(): void {
-    let student = history.state.student || null;
-    if (student && 'goalsCtrl' in student) {
-      this.addCheckboxesFromCache(student.goalsCtrl);
-    } else {
-      this.addCheckboxes();
-    }
+    // let student = history.state.student || null;
+    // if (student && 'goalsCtrl' in student) {
+    //   this.addCheckboxesFromCache(student.goalsCtrl);
+    // } else {
+    //   this.addCheckboxes();
+    // }
   }
 
-  get goalsFormArray() {
-    return this.formGroup.controls.goalsCtrl as FormArray;
-  }
+  // get goalsFormArray() {
+  //   return this.formGroup.controls.goalsCtrl as FormArray;
+  // }
 
-  entry(obj) {
-    return Object.entries(obj)[0];
-  }
-  handleCheck(goal, isChecked, index) {
-    this.goalsFormArray.controls[index].patchValue({ [goal]: !isChecked });
-  }
+  // entry(obj) {
+  //   return Object.entries(obj)[0];
+  // }
+  // handleCheck(goal, isChecked, index) {
+  //   this.goalsFormArray.controls[index].patchValue({ [goal]: !isChecked });
+  // }
 
-  private addCheckboxesFromCache(goals) {
-    goals.forEach((goal) => {
-      let entries = this.entry(goal);
-      this.goalsFormArray.push(new FormControl({ [entries[0]]: entries[1] }));
-    });
-  }
+  // private addCheckboxesFromCache(goals) {
+  //   goals.forEach((goal) => {
+  //     let entries = this.entry(goal);
+  //     this.goalsFormArray.push(new FormControl({ [entries[0]]: entries[1] }));
+  //   });
+  // }
 
-  private addCheckboxes() {
-    this.goalsData.forEach(({ goal, isChecked }) =>
-      this.goalsFormArray.push(new FormControl({ [goal]: isChecked }))
-    );
-  }
+  // private addCheckboxes() {
+  //   this.goalsData.forEach(({ goal, isChecked }) =>
+  //     this.goalsFormArray.push(new FormControl({ [goal]: isChecked }))
+  //   );
+  // }
 }
