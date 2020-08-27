a71c04c77d462322212e62d52f464c8e653cf8ff
danyrtmn <dany.rtmn@gmail.com>
2020-08-27 14:47:56 +0300

    summary step update

--- apps/joie/src/app/onboarding/onboarding-student/components/summary-step/summary-step.component.ts
+++ apps/joie/src/app/onboarding/onboarding-student/components/summary-step/summary-step.component.ts
@@ -8,36 +8,36 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class SummaryStepComponent implements OnInit {
   student; // todo add interface
-  name = 'heregoesthename';
-  dataToPrint;
+  name = 'NAME';
+  // dataToPrint;
   constructor(public activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.student = history.state.student;
-    console.log(this.student);
-    this.getDataForPrinting();
+    // console.log(this.student);
+    // this.getDataForPrinting();
   }
 
-  mapAndFilter(array: {}[]): string[] {
-    return array
-      .map((item) => {
-        let itemKeyVal = Object.entries(item)[0];
-        return itemKeyVal[1] ? itemKeyVal[0] : null;
-      })
-      .filter(Boolean);
-  }
+  // mapAndFilter(array: {}[]): string[] {
+  //   return array
+  //     .map((item) => {
+  //       let itemKeyVal = Object.entries(item)[0];
+  //       return itemKeyVal[1] ? itemKeyVal[0] : null;
+  //     })
+  //     .filter(Boolean);
+  // }
 
-  getDataForPrinting() {
-    this.dataToPrint = {
-      // goals: this.mapAndFilter(this.student.goalsCtrl),
-      // subgoals: [],
-      sessionTypes: this.mapAndFilter(this.student.sessionTypesCtrl),
-    };
+  // getDataForPrinting() {
+  //   this.dataToPrint = {
+  //     // goals: this.mapAndFilter(this.student.goalsCtrl),
+  //     // subgoals: [],
+  //     sessionTypes: this.mapAndFilter(this.student.sessionTypesCtrl),
+  //   };
 
-    // Object.values(this.student.subgoalsCtrl).forEach((subgoal) => {
-    //   this.dataToPrint.subgoals.push(this.mapAndFilter(subgoal['subgoals']));
-    // });
-  }
+  //   // Object.values(this.student.subgoalsCtrl).forEach((subgoal) => {
+  //   //   this.dataToPrint.subgoals.push(this.mapAndFilter(subgoal['subgoals']));
+  //   // });
+  // }
 
   submitData() {
     // todo create api service for this purpose and call it here
