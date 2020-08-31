33ac8dfc95916d9a9441d733bafb32127a01e9b6
yinonov <yinon@hotmail.com>
2020-08-31 14:25:01 +0300

    style & layout references adjusted

--- apps/joie/src/app/home/containers/home-dashboard/home-dashboard.component.ts
+++ apps/joie/src/app/home/containers/home-dashboard/home-dashboard.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { homePillars } from './home-pillars';
 
 @Component({
   templateUrl: './home-dashboard.component.html',
-  styleUrls: ['./home-dashboard.component.scss']
+  styleUrls: ['./home-dashboard.component.scss'],
 })
 export class HomeDashboardComponent implements OnInit {
+  homePillars = homePillars;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
