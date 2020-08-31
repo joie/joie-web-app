7a8c9a214a22eced2e4c092a47f15f3a37e5b58a
yinonov <yinon@hotmail.com>
2020-09-01 01:01:20 +0300

    removed unsupported sign in methods

--- apps/joie/src/app/shared/shared.module.ts
+++ apps/joie/src/app/shared/shared.module.ts
@@ -3,26 +3,19 @@ import { CommonModule } from '@angular/common';
 import { QuicklinkModule } from 'ngx-quicklink';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PaymentSourceComponent } from './components/payment-source/payment-source.component';
-import { VideoComponent } from './components/video/video.component';
 import { DialogRouterComponent } from './components/dialog-router/dialog-router.component';
 import { FigureImageComponent } from './components/figure-image/figure-image.component';
 
 import { MaterialModule } from '../core/material.module';
 
 @NgModule({
-  declarations: [
-    PaymentSourceComponent,
-    VideoComponent,
-    DialogRouterComponent,
-    FigureImageComponent,
-  ],
+  declarations: [PaymentSourceComponent, DialogRouterComponent, FigureImageComponent],
   imports: [CommonModule, QuicklinkModule, ReactiveFormsModule, MaterialModule],
   exports: [
     CommonModule,
     QuicklinkModule,
     ReactiveFormsModule,
     PaymentSourceComponent,
-    VideoComponent,
     DialogRouterComponent,
     FigureImageComponent,
     // ! remove in favour of individual importing
