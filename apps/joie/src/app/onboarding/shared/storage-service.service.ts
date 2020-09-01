c423fc4414f19b811d317083e05593059c3dc10a
danyrtmn <dany.rtmn@gmail.com>
2020-09-01 17:42:05 +0300

    implementing storage service

---
+++ apps/joie/src/app/onboarding/shared/storage-service.service.ts
@@ -0,0 +1,32 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { take } from 'rxjs/operators';
+
+export const USER_ONBOARDING = 'user-onboarding';
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageServiceService {
+  constructor(private storage: StorageMap) {}
+
+  setItemSubscribe(featureKey, newValue): void {
+    this.storage
+      .get(featureKey)
+      .pipe(take(1))
+      .subscribe((prevValue: object) => {
+        if (!prevValue) {
+          const subscription = this.storage.set(featureKey, newValue).pipe(take(1)).subscribe();
+        } else {
+          const valueEntries = Object.entries(newValue)[0];
+          const subscription = this.storage
+            .set(featureKey, { [valueEntries[0]]: valueEntries[1] })
+            .pipe(take(1))
+            .subscribe();
+        }
+      });
+  }
+  getItem(featureKey): Observable<any> {
+    return this.storage.get(featureKey);
+  }
+}
