a2fb137d6094c95e3e500add09c207ecd8d7ffb4
danyrtmn <dany.rtmn@gmail.com>
2020-09-01 18:29:27 +0300

    pillars step: close subscription onDestroy

--- apps/joie/src/app/onboarding/onboarding-student/components/pillar-step/pillar-step.component.ts
+++ apps/joie/src/app/onboarding/onboarding-student/components/pillar-step/pillar-step.component.ts
@@ -1,7 +1,7 @@
 import { skip } from 'rxjs/operators';
 import { Pillar } from '../../../../sessions/models/session';
-import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
+import { Component, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 import { StudentOnboardingService } from '../../service/student-onboarding.service';
 import { atLeastOneIsCheckedValidator } from '../../../validators/atLeastOnIsChecked';
 import { StorageServiceService, USER_ONBOARDING } from '../../../shared/storage-service.service';
@@ -12,9 +12,10 @@ export const PILLARS = 'pillars';
   templateUrl: './pillar-step.component.html',
   styleUrls: ['./pillar-step.component.scss'],
 })
-export class PillarStepComponent {
+export class PillarStepComponent implements OnDestroy {
   formGroup: FormGroup;
   pillarEnum = Pillar;
+  formValueChanges$;
 
   get pillarKeys() {
     return Object.keys(Pillar);
@@ -32,20 +33,21 @@ export class PillarStepComponent {
     this.formGroup = this._formBuilder.group({
       pillars: new FormArray([], atLeastOneIsCheckedValidator()),
     });
-    // onboardingService.addCheckboxes(this.pillarKeys, this.pillarFormArray, this.pillarEnum);
     this.fillFormArray();
 
     //subscribe to value chan ges skip form init
-    this.formGroup.valueChanges
+    this.formValueChanges$ = this.formGroup.valueChanges
       .pipe(skip(this.pillarKeys.length))
       .subscribe(() => this.storage.setItemSubscribe(USER_ONBOARDING, this.submit()));
+  }
+  ngOnDestroy(): void {
+    this.formValueChanges$.unsubscribe();
   }
 
   fillFormArray() {
     this.storage.getItem(USER_ONBOARDING).subscribe((res) => {
       if (res && res[PILLARS]) {
         this.formGroup.controls[PILLARS].markAsTouched();
-        // this.addPillarCheckboxesFromCache(res[PILLARS]);
         this.onboardingService.addCheckboxes(
           this.pillarKeys,
           this.pillarFormArray,
@@ -53,7 +55,6 @@ export class PillarStepComponent {
           res[PILLARS]
         );
       } else {
-        // this.addPillarCheckboxes();
         this.onboardingService.addCheckboxes(
           this.pillarKeys,
           this.pillarFormArray,
@@ -73,19 +74,5 @@ export class PillarStepComponent {
       .map((checked, i) => (checked ? this.pillarEnum[this.pillarKeys[i]] : null))
       .filter((v) => v !== null);
     return { pillars: selectedPillarTitles };
-  }
-
-  private addPillarCheckboxes() {
-    this.pillarKeys.forEach(() => this.pillarFormArray.push(new FormControl('dick')));
-  }
-
-  private addPillarCheckboxesFromCache(pillars) {
-    this.pillarKeys.forEach((key) => {
-      if (pillars.includes(this.pillarEnum[key])) {
-        this.pillarFormArray.push(new FormControl(true));
-      } else {
-        this.pillarFormArray.push(new FormControl(false));
-      }
-    });
   }
 }
