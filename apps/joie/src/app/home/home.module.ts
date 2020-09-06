8d29724ab56f3d0218c29f3b1e09b9e16c96f7b7
danyrtmn <dany.rtmn@gmail.com>
2020-09-06 16:32:59 +0300

    replacing pillars on home page

--- apps/joie/src/app/home/home.module.ts
+++ apps/joie/src/app/home/home.module.ts
@@ -7,15 +7,20 @@ import { MaterialModule } from '../core/material.module';
 import { KalturaPlayerModule } from '../kaltura-player/kaltura-player.module';
 import { SharedModule } from '../shared/shared.module';
 import { PillarKeywordEmphasisPipe } from './pipes/pillar-keyword-emphasis/pillar-keyword-emphasis.pipe';
+import { PillarListModule } from '../pillar-list/pillar-list.module';
 
 @NgModule({
-  declarations: [HomeDashboardComponent, PillarKeywordEmphasisPipe],
+  declarations: [
+    HomeDashboardComponent,
+    // PillarKeywordEmphasisPipe //todo moved to pillar-list module
+  ],
   imports: [
     SharedModule,
     HomeRoutingModule,
     SessionListModule,
     MaterialModule,
     KalturaPlayerModule,
+    PillarListModule,
   ],
 })
 export class HomeModule {}
