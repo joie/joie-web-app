5b2009b167a71b8b614bde6bcee2f08d134aff35
danyrtmn <dany.rtmn@gmail.com>
2020-09-07 20:05:10 +0300

    activity-step changeDetectionStrategy onPush->default

--- apps/joie/src/app/onboarding/onboarding-student/components/activities-step/activities-step.component.ts
+++ apps/joie/src/app/onboarding/onboarding-student/components/activities-step/activities-step.component.ts
@@ -18,7 +18,7 @@ import { merge } from 'lodash';
   selector: 'app-activities-step',
   templateUrl: './activities-step.component.html',
   styleUrls: ['./activities-step.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivitiesStepComponent implements AfterViewInit {
   @ViewChildren(ActivitiesBoxComponent) activityBoxes: QueryList<ActivitiesBoxComponent>;
