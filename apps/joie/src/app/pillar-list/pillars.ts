7434d2c354815f5e5ba3f4ce7a72f62b35872cbb
yinonov <yinon@hotmail.com>
2020-09-08 22:48:09 +0300

    maintenence

--- apps/joie/src/app/pillar-list/pillars.ts
+++ apps/joie/src/app/pillar-list/pillars.ts
@@ -2,27 +2,27 @@ import { Pillar } from '../sessions/models';
 
 export const pillars = [
   {
-    imgUrl: '/assets/images/movement.png',
+    icon: 'movement',
     title: Pillar.Movement,
     description: 'Exercise, Energy, Diet, and Nutrition.',
   },
   {
-    imgUrl: '/assets/images/emotions.png',
+    icon: 'emotions',
     title: Pillar.Emotions,
     description: 'Self-Regulation, Self-care, Relaxation, Stress Reduction, and Inner Strength.',
   },
   {
-    imgUrl: '/assets/images/connections.png',
+    icon: 'connections',
     title: Pillar.Connections,
     description: 'Social interactions, Friendships, Parenting, and Relationships.',
   },
   {
-    imgUrl: '/assets/images/spirit.png',
+    icon: 'spirit',
     title: Pillar.Spirit,
     description: 'Seek Meaning, Individual Purpose, Faith, Values, Ethics and Morals.',
   },
   {
-    imgUrl: '/assets/images/professional.png',
+    icon: 'professional',
     title: Pillar.Professional,
     description: 'Satisfaction at Work, Professional Development, and Financial Stability.',
   },
