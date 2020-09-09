973a5c1a798be001194175e7cabcd71319907cc9
danyrtmn <dany.rtmn@gmail.com>
2020-09-09 14:40:58 +0300

    refactoring to use new global layout

--- apps/joie/src/app/pillar-list/pillar-list.component.ts
+++ apps/joie/src/app/pillar-list/pillar-list.component.ts
@@ -25,7 +25,6 @@ export class PillarListComponent {
   controlKey = USER_ONBOARDING + '-' + PILLARS;
   @Input() selectable = false;
   @Input() descriptions = false;
-  @Input() layoutClass;
 
   get subForm() {
     return this.form;
