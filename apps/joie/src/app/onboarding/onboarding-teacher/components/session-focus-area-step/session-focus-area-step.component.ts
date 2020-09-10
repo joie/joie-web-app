d54d091d7f8e2b8b15cbb86b8f5827ad68ac7ef0
danyrtmn <dany.rtmn@gmail.com>
2020-09-10 13:34:46 +0300

    refactoring checkboxes approach foocus-area-step

--- apps/joie/src/app/onboarding/onboarding-teacher/components/session-focus-area-step/session-focus-area-step.component.ts
+++ apps/joie/src/app/onboarding/onboarding-teacher/components/session-focus-area-step/session-focus-area-step.component.ts
@@ -1,69 +1,98 @@
-import { Component } from '@angular/core';
+import { OnboardingService } from './../../../shared/onboarding.service';
+import { AgeGroups } from './../../../../models/teacher.model';
+import { Component, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 import { notMoreThanOneIsCheckedValidator } from '../../../validators/notMoreThanOneIsSelected';
 import { atLeastOneIsCheckedValidator } from '../../../validators/atLeastOnIsChecked';
 // todo add validation error messages after refactoring the checkboxes part
 
+const GROUPS = 'ageGroups';
 @Component({
   selector: 'app-session-focus-area-step',
   templateUrl: './session-focus-area-step.component.html',
   styleUrls: ['./session-focus-area-step.component.scss'],
 })
-export class SessionFocusAreaStepComponent {
-  formGroup: FormGroup;
-  focusGroupsData = [
-    { group: 'Children (6-14)', isChecked: false },
-    { group: 'Youth (15-24)', isChecked: false },
-    { group: 'Adults (25-64)', isChecked: false }, //todo when refactoring make this checkbox selected by default - adult is the most popular choice
-    { group: 'Eldery (65+)', isChecked: false },
-    { group: 'All of the above', isChecked: false },
-  ];
-  constructor(private _formBuilder: FormBuilder) {
-    this.formGroup = this._formBuilder.group({
-      sessionArea: ['', [Validators.required, Validators.minLength(10)]],
-      ageGroups: new FormArray(
-        [],
-        [atLeastOneIsCheckedValidator(), notMoreThanOneIsCheckedValidator()]
-      ),
-    });
+export class SessionFocusAreaStepComponent implements OnDestroy {
+  // formGroup: FormGroup;
+  // focusGroupsData = [
+  //   { group: 'Children (6-14)', isChecked: false },
+  //   { group: 'Youth (15-24)', isChecked: false },
+  //   { group: 'Adults (25-64)', isChecked: false }, //todo when refactoring make this checkbox selected by default - adult is the most popular choice
+  //   { group: 'Eldery (65+)', isChecked: false },
+  //   { group: 'All of the above', isChecked: false },
+  // ];
+  form: FormGroup;
+  groupsEnum = AgeGroups;
 
-    let teacher = history.state.teacher || null;
-    if (teacher && 'ageGroups' in teacher) {
-      this.initFormWithCachedData(teacher);
-    } else {
-      this.addCheckboxes();
-    }
+  get groupKeys() {
+    return Object.keys(this.groupsEnum);
   }
 
   get groupsFormArray() {
-    return this.formGroup.controls.ageGroups as FormArray;
+    return this.form.get(GROUPS) as FormArray;
   }
 
-  entry(obj) {
-    return Object.entries(obj)[0];
+  get values() {
+    return this.form.value[GROUPS].map((checked, i) =>
+      checked ? this.groupsEnum[this.groupKeys[i]] : null
+    ).filter((v) => v !== null);
   }
 
-  handleCheck(group, isChecked, index) {
-    this.groupsFormArray.controls[index].patchValue({
-      [group]: !isChecked,
+  constructor(private _formBuilder: FormBuilder, private onboardingService: OnboardingService) {
+    this.form = this._formBuilder.group({
+      sessionArea: ['', [Validators.required, Validators.minLength(10)]],
+      ageGroups: new FormArray(
+        []
+        // [atLeastOneIsCheckedValidator(), notMoreThanOneIsCheckedValidator()]
+      ),
     });
+
+    this.onboardingService.addCheckboxes(this.groupKeys, this.groupsFormArray);
+
+    // let teacher = history.state.teacher || null;
+    // if (teacher && 'ageGroups' in teacher) {
+    //   this.initFormWithCachedData(teacher);
+    // } else {
+    //   this.addCheckboxes();
+    // }
   }
 
-  private addCheckboxes() {
-    this.focusGroupsData.forEach(({ group, isChecked }) =>
-      this.groupsFormArray.push(new FormControl({ [group]: isChecked }))
-    );
+  log() {
+    console.log(this.form.value);
+    console.log(this.values);
   }
 
-  private addCheckboxesFromCache(groups) {
-    groups.forEach((group) => {
-      let entries = this.entry(group);
-      this.groupsFormArray.push(new FormControl({ [entries[0]]: entries[1] }));
-    });
-  }
+  ngOnDestroy(): void {}
 
-  private initFormWithCachedData(teacher) {
-    this.formGroup.controls['sessionArea'].setValue(teacher.sessionArea);
-    this.addCheckboxesFromCache(teacher.ageGroups);
-  }
+  // get groupsFormArray() {
+  //   return this.form.controls.ageGroups as FormArray;
+  // }
+
+  // entry(obj) {
+  // return Object.entries(obj)[0];
+  // }
+
+  // handleCheck(group, isChecked, index) {
+  //   this.groupsFormArray.controls[index].patchValue({
+  //     [group]: !isChecked,
+  //   });
+  // }
+
+  // private addCheckboxes() {
+  //   this.focusGroupsData.forEach(({ group, isChecked }) =>
+  //     this.groupsFormArray.push(new FormControl({ [group]: isChecked }))
+  //   );
+  // }
+
+  // private addCheckboxesFromCache(groups) {
+  //   groups.forEach((group) => {
+  //     let entries = this.entry(group);
+  //     this.groupsFormArray.push(new FormControl({ [entries[0]]: entries[1] }));
+  //   });
+  // }
+
+  // private initFormWithCachedData(teacher) {
+  //   this.formGroup.controls['sessionArea'].setValue(teacher.sessionArea);
+  //   this.addCheckboxesFromCache(teacher.ageGroups);
+  // }
 }
