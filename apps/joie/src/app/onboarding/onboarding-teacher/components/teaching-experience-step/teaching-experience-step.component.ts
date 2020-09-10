84e3dd45063bd3e90254cc5210a7921d8a2e5252
danyrtmn <dany.rtmn@gmail.com>
2020-09-10 15:15:00 +0300

    experience-step: formService, validation, getting/setting cache

--- apps/joie/src/app/onboarding/onboarding-teacher/components/teaching-experience-step/teaching-experience-step.component.ts
+++ apps/joie/src/app/onboarding/onboarding-teacher/components/teaching-experience-step/teaching-experience-step.component.ts
@@ -1,41 +1,80 @@
+import {
+  TEACHER_ONBOARDING,
+  StorageServiceService,
+} from './../../../shared/storage-service.service';
+import { TeacherOnboardingFormService } from './../../services/teacher-onboarding-form.service';
 import { AuthService } from './../../../../auth-state/services/auth/auth.service';
 import { OnboardingService } from './../../../shared/onboarding.service';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
-export const TEACHING_EXPERIENCE = 'experience';
+export const TEACHING_EXPERIENCE = 'teaching-experience';
+export const EXPERIENCE = 'experience';
 @Component({
   selector: 'app-teaching-experience-step',
   templateUrl: './teaching-experience-step.component.html',
   styleUrls: ['./teaching-experience-step.component.scss'],
 })
-export class TeachingExperienceStepComponent implements OnInit {
-  formGroup: FormGroup;
+export class TeachingExperienceStepComponent implements OnDestroy {
+  form: FormGroup;
+  controlKey = TEACHER_ONBOARDING + '-' + TEACHING_EXPERIENCE;
+  formValueChanges$: Subscription;
 
-  get teachingExp() {
-    return this.formGroup.get(TEACHING_EXPERIENCE);
+  get experienceControl() {
+    return this.form.get(EXPERIENCE);
   }
 
   constructor(
     public authService: AuthService,
     private _formBuilder: FormBuilder,
     public activatedRoute: ActivatedRoute,
-    public onboardingService: OnboardingService
+    public onboardingService: OnboardingService,
+    private formService: TeacherOnboardingFormService,
+    private storage: StorageServiceService
   ) {
-    this.formGroup = this._formBuilder.group({
-      experience: ['', [Validators.required, Validators.minLength(50), Validators.maxLength(300)]],
+    this.form = this._formBuilder.group({
+      [EXPERIENCE]: [
+        '',
+        [Validators.required, Validators.minLength(50), Validators.maxLength(300)],
+      ],
+    });
+
+    this.initForm();
+  }
+
+  ngOnDestroy() {
+    this.formValueChanges$.unsubscribe();
+  }
+
+  initForm() {
+    this.formService.setControl([EXPERIENCE, new FormControl()]);
+
+    this.getCache();
+
+    this.subscribeToValueChanges();
+  }
+
+  getCache() {
+    this.storage.getItem(this.controlKey).subscribe((cacheValue) => {
+      if (cacheValue) {
+        this.form.patchValue(cacheValue);
+      }
     });
   }
 
-  ngOnInit() {
-    let teacher = history.state.teacher;
-    if ('experience' in history.state.teacher) {
-      this.initFormWithCachedData(teacher);
-    }
+  subscribeToValueChanges() {
+    this.formValueChanges$ = this.form.valueChanges.subscribe((value) => {
+      this.formService.form.patchValue(value);
+      if (this.form.valid) {
+        // not caching invalid value
+        this.storage.setItemSubscribe(this.controlKey, value);
+      }
+    });
   }
 
-  private initFormWithCachedData(teacher) {
-    this.formGroup.controls['experience'].setValue(teacher.experience);
+  isValid() {
+    return this.form.valid;
   }
 }
