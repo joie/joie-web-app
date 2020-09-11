ca95fa0e2929680245a1dfeab82c24f35a21044d
yinonov <yinon@hotmail.com>
2020-09-11 15:30:27 +0300

    raise the bar for typescript checks, linting...

--- apps/joie/src/app/common/components/form-array-table/form-array-table.component.ts
+++ apps/joie/src/app/common/components/form-array-table/form-array-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormArray } from '@angular/forms';
 
 @Component({
@@ -6,9 +6,22 @@ import { FormArray } from '@angular/forms';
   templateUrl: './form-array-table.component.html',
   styleUrls: ['./form-array-table.component.scss'],
 })
-export class FormArrayTableComponent {
-  @Input() values: FormArray;
+export class FormArrayTableComponent implements OnChanges {
+  @Input() values: string[];
+  // @Input()
+  // get values(): any[] {
+  //   return this._values;
+  // }
+  // set values(value: FormArray) {
+  //   this._values = value && value.value;
+  // }
+  // private _values;
+
   @Output() remove = new EventEmitter();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.values);
+  }
 
   displayedColumns: string[] = ['value', 'action'];
 }
--- apps/joie/src/app/sessions/sessions.module.ts
+++ apps/joie/src/app/sessions/sessions.module.ts
@@ -7,7 +7,7 @@ import { SessionsRoutingModule } from './sessions-routing.module';
 
 import { SessionsDashboardComponent } from './containers/sessions-dashboard/sessions-dashboard.component';
 import { SessionEnrollDialogComponent } from './components/session-enroll-dialog/session-enroll-dialog.component';
-import { SessionDetailsComponent } from './components/session-details/session-details.component';
+import { SessionDetailsComponent } from './containers/session-details/session-details.component';
 import { SessionCardComponent } from './components/session-card/session-card.component';
 import { KalturaPlayerModule } from '../kaltura-player/kaltura-player.module';
 import { SessionListModule } from '../session-list/session-list.module';
