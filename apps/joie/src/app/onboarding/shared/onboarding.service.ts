486ff6b3cbf7d0624de3876d110e5dcb490cb834
danyrtmn <dany.rtmn@gmail.com>
2020-09-16 11:53:39 +0300

    error message

--- apps/joie/src/app/onboarding/shared/onboarding.service.ts
+++ apps/joie/src/app/onboarding/shared/onboarding.service.ts
@@ -16,12 +16,15 @@ export class OnboardingService {
     switch (errorKeys[0]) {
       case 'required':
         return 'required';
+
       case 'email':
         return 'Please enter a valid email address.';
+
       case 'minlength':
         return `Should be longer than ${
           errors[errorKeys[0]].requiredLength
         } symbols. Actual length ${errors[errorKeys[0]].actualLength}`;
+
       case 'maxlength':
         return `The text entered exceeds the maximum length. ${
           errors[errorKeys[0]].requiredLength
@@ -29,12 +32,17 @@ export class OnboardingService {
 
       case 'requireCheckboxToBeChecked':
         return 'At least one should be checked';
+
       case 'requireNotMoreThanOneCheckboxToBeChecked':
         return 'Not more than one should be checked';
+
+      case 'exceedesWordLimit':
+        return `The text entered exceeds the maximum word limit ${errors.actualWordsCount}/${errors.limit}`;
 
       case 'pattern':
         const pattern = errors.pattern.requiredPattern;
         return this.getPatternErrorMessage(pattern);
+
       default:
         console.log(formControl);
         console.log(errors);
