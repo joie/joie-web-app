3077dde49fb1f209a2d52f028e9bcce4cda2c800
yinonov <yinon@hotmail.com>
2020-09-30 17:56:50 +0300

    refactor(breakpoints): updates to breakpoint handling

--- apps/joie/src/app/mission/containers/mission/mission.component.ts
+++ apps/joie/src/app/mission/containers/mission/mission.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Breakpoints } from '@angular/cdk/layout';
+import { fromEventPattern } from 'rxjs';
+import { pluck, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-mission',
   templateUrl: './mission.component.html',
   styleUrls: ['./mission.component.scss'],
 })
-export class MissionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class MissionComponent {
+  isLargeBreakpoint$ = fromEventPattern((handler) =>
+    window.matchMedia(Breakpoints.Web).addEventListener('change', handler)
+  ).pipe(startWith(window.matchMedia(Breakpoints.Web)), pluck('matches'));
 }
