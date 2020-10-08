927ebd4616cc27eb8394741ff1386354a10c9eb5
widesea30 <widesea392@yahoo.com>
2020-10-08 14:24:25 +0200

    issue-142: 6,7,9,11,12,13

--- apps/joie/src/app/onboarding/onboarding-teacher/teacher-onboarding.enums.ts
+++ apps/joie/src/app/onboarding/onboarding-teacher/teacher-onboarding.enums.ts
@@ -35,23 +35,23 @@ export const AgeGroupsLiteralsMap = new Map([
   [AgeGroups.Youth, 'youth (15-24'],
   [AgeGroups.Adults, 'adults (25-64'],
   [AgeGroups.Eldery, 'eldery (65+)'],
-  [AgeGroups.Eldery, 'all of the above'],
+  [AgeGroups.All, 'all of the above'],
 ]);
 
 export enum SessionTypes {
   OnDemandSessionOrLecture = 'onDemandSessionOrLecture',
-  // OnDemandCourse = 'onDemandCourse', //todo courses are not available at joie yet https://github.com/joie/joie-web-app/issues/59#issue-689403295 (bullet 4)
+  OnDemandCourse = 'onDemandCourse', //todo courses are not available at joie yet https://github.com/joie/joie-web-app/issues/59#issue-689403295 (bullet 4)
   LiveGroupSession = 'liveGroupSession',
-  // LiveGroupCourse = 'liveGroupCourse',
+  LiveGroupCourse = 'liveGroupCourse',
   LiveLecture = 'liveGroupLecture',
   Coaching = 'liveCoaching',
 }
 
 export const SessionTypesLiteralsMap = new Map([
   [SessionTypes.OnDemandSessionOrLecture, 'on-demand session or a lecture'],
-  // [SessionTypes.OnDemandCourse, 'on-demand course'],
+  [SessionTypes.OnDemandCourse, 'on-demand course'],
   [SessionTypes.LiveGroupSession, 'live group session'],
-  // [SessionTypes.LiveGroupCourse, 'live group course'],
+  [SessionTypes.LiveGroupCourse, 'live group course'],
   [SessionTypes.LiveLecture, 'live lecture'],
   [SessionTypes.Coaching, 'live 1:1 coaching'],
 ]);
