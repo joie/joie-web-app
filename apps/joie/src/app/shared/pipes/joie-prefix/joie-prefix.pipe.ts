4f19bd15e2b39e20cc62996e7d1a540050f3878d
widesea30 <widesea392@yahoo.com>
2020-10-21 15:31:03 +0200

    add default value in joiePrefix

--- apps/joie/src/app/shared/pipes/joie-prefix/joie-prefix.pipe.ts
+++ apps/joie/src/app/shared/pipes/joie-prefix/joie-prefix.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'joiePrefix',
 })
 export class JoiePrefixPipe implements PipeTransform {
-  transform(value: unknown, highlight: boolean): string {
+  transform(value: unknown, highlight: boolean = true): string {
     console.log(highlight);
     const pillar = highlight ? `<span class="text-color-primary">${value}</span>` : value;
     return `Joie${pillar}`;
