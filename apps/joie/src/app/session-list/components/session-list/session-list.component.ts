dae993339821dccfcb7ffb80ee0c8cf469e77a63
yinonov <yinon@hotmail.com>
2020-11-25 22:50:53 +0200

    extract card to own component

--- apps/joie/src/app/session-list/components/session-list/session-list.component.ts
+++ apps/joie/src/app/session-list/components/session-list/session-list.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import { Location } from '@angular/common';
 import { QueryFn } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 import { SessionsService } from '../../../services/sessions/sessions.service';
-import { AuthFacade } from './../../../auth/services/auth.facade';
 import { Session } from '../../../../../../../libs/schemes/src';
 
 @Component({
@@ -15,11 +12,10 @@ import { Session } from '../../../../../../../libs/schemes/src';
 })
 export class SessionListComponent implements OnInit {
   @Input() queryFn: QueryFn;
-  uid$ = this.authFacade.uid$;
 
   sessions$: Observable<Session[]>;
 
-  sessions: any[] = [];
+  sessions: Session[] = [];
 
   firstInResponse: any = [];
   lastInResponse: any = [];
@@ -29,97 +25,102 @@ export class SessionListComponent implements OnInit {
   paginationClickedCount = 0;
   pageCount = 6;
 
-  constructor(
-    private sessionsService: SessionsService,
-    private authFacade: AuthFacade,
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private location: Location,
-  ) {}
+  constructor(private sessionsService: SessionsService) {}
 
   ngOnInit(): void {
     this.getItems();
   }
 
   getItems() {
-    this.sessionsService.getSessionsData('sessions', this.pageCount, this.queryFn).subscribe((response: any) => {
-      if (!response.length) {
-        return false;
-      }
-      this.firstInResponse = response[0].payload.doc;
-      this.lastInResponse = response[response.length - 1].payload.doc;
+    this.sessionsService.getSessionsData('sessions', this.pageCount, this.queryFn).subscribe(
+      (response: any) => {
+        if (!response.length) {
+          return false;
+        }
+        this.firstInResponse = response[0].payload.doc;
+        this.lastInResponse = response[response.length - 1].payload.doc;
 
-      this.sessions = [];
-      for (let item of response) {
-        this.sessions.push(item.payload.doc.data());
-      }
+        this.sessions = [];
+        for (const item of response) {
+          this.sessions.push(item.payload.doc.data());
+        }
 
-      this.prevStrtAt = [];
-      this.disable_next = false;
-      this.disable_prev = true;
+        this.prevStrtAt = [];
+        this.disable_next = false;
+        this.disable_prev = true;
 
-      this.push_prev_startAt(this.firstInResponse);
-    }, error => {
-      console.log(error);
-    });
+        this.push_prev_startAt(this.firstInResponse);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
   }
 
   nextPage() {
     this.disable_next = true;
-    this.sessionsService.getSessionsNext('sessions', this.pageCount, this.lastInResponse, this.queryFn).subscribe((response: any) => {
-      if (!response.length) {
-        this.disable_next = true;
-        return;
-      }
-      this.firstInResponse = response[0].payload.doc;
-      this.lastInResponse = response[response.length - 1].payload.doc;
-      this.sessions = [];
-      for (let item of response) {
-        this.sessions.push(item.payload.doc.data());
-      }
+    this.sessionsService.getSessionsNext('sessions', this.pageCount, this.lastInResponse, this.queryFn).subscribe(
+      (response: any) => {
+        if (!response.length) {
+          this.disable_next = true;
+          return;
+        }
+        this.firstInResponse = response[0].payload.doc;
+        this.lastInResponse = response[response.length - 1].payload.doc;
+        this.sessions = [];
+        for (const item of response) {
+          this.sessions.push(item.payload.doc.data());
+        }
 
-      this.paginationClickedCount++;
-      this.push_prev_startAt(this.firstInResponse);
-      if (response.length < this.pageCount) {
-        // disable next button if data fetched is less than 5 - means no more data left to load
-        // because limit ti get data is set to 5
-        this.disable_next = true;
-      } else {
-        this.disable_next = false;
-      }
-      this.disable_prev = false;
-    }, error => {
-      console.log(error);
-    });
+        this.paginationClickedCount++;
+        this.push_prev_startAt(this.firstInResponse);
+        if (response.length < this.pageCount) {
+          // disable next button if data fetched is less than 5 - means no more data left to load
+          // because limit ti get data is set to 5
+          this.disable_next = true;
+        } else {
+          this.disable_next = false;
+        }
+        this.disable_prev = false;
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
   }
 
   prevPage() {
     this.disable_prev = true;
-    this.sessionsService.getSessionsPrev('sessions', this.pageCount, this.get_prev_startAt(), this.firstInResponse, this.queryFn).subscribe((response: any) => {
-      this.firstInResponse = response[0].payload.doc;
-      this.lastInResponse = response[response.length - 1].payload.doc;
+    this.sessionsService
+      .getSessionsPrev('sessions', this.pageCount, this.get_prev_startAt(), this.firstInResponse, this.queryFn)
+      .subscribe(
+        (response: any) => {
+          this.firstInResponse = response[0].payload.doc;
+          this.lastInResponse = response[response.length - 1].payload.doc;
 
-      this.sessions = [];
-      for (let item of response) {
-        this.sessions.push(item.payload.doc.data());
-      }
+          this.sessions = [];
+          for (const item of response) {
+            this.sessions.push(item.payload.doc.data());
+          }
 
-      // maintaing paginationClickedCount no.
-      this.paginationClickedCount--;
+          // maintaing paginationClickedCount no.
+          this.paginationClickedCount--;
 
-      // pop not required value in array
-      this.pop_prev_startAt(this.firstInResponse);
+          // pop not required value in array
+          this.pop_prev_startAt(this.firstInResponse);
 
-      // enable buttons again
-      if (this.paginationClickedCount === 0) {
-        this.disable_prev = true;
-      } else {
-        this.disable_prev = false;
-      }
-      this.disable_next = false;
-    }, error => {
-      console.log(error);
-    });
+          // enable buttons again
+          if (this.paginationClickedCount === 0) {
+            this.disable_prev = true;
+          } else {
+            this.disable_prev = false;
+          }
+          this.disable_next = false;
+        },
+        (error) => {
+          console.log(error);
+        },
+      );
   }
 
   push_prev_startAt(prevFirstDoc) {
@@ -127,7 +128,7 @@ export class SessionListComponent implements OnInit {
   }
 
   pop_prev_startAt(prevFirstDoc) {
-    this.prevStrtAt.forEach(element => {
+    this.prevStrtAt.forEach((element) => {
       if (prevFirstDoc.id === element.id) {
         element = null;
       }
@@ -135,7 +136,7 @@ export class SessionListComponent implements OnInit {
   }
 
   get_prev_startAt() {
-    if (this.prevStrtAt.length > (this.paginationClickedCount + 1)) {
+    if (this.prevStrtAt.length > this.paginationClickedCount + 1) {
       this.prevStrtAt.splice(this.prevStrtAt.length - 2, this.prevStrtAt.length - 1);
     }
     return this.prevStrtAt[this.paginationClickedCount - 1];
--- apps/joie/src/app/session-list/session-list.module.ts
+++ apps/joie/src/app/session-list/session-list.module.ts
@@ -6,17 +6,11 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { SessionListRoutingModule } from './session-list-routing.module';
+import { SessionCardComponent } from './components/session-card/session-card/session-card.component';
 
 @NgModule({
-  declarations: [SessionListComponent],
-  imports: [
-    SharedModule,
-    SessionListRoutingModule,
-    MatCardModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatTableModule,
-  ],
+  declarations: [SessionListComponent, SessionCardComponent],
+  imports: [SharedModule, SessionListRoutingModule, MatCardModule, MatPaginatorModule, MatSortModule, MatTableModule],
   exports: [SessionListComponent],
 })
 export class SessionListModule {}
