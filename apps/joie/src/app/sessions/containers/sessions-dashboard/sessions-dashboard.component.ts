c05b68cd2032baa0bc840b9fb86b96dc4397da76
yinonov <yinon@hotmail.com>
2020-11-28 09:57:47 +0200

    removed commented code

--- apps/joie/src/app/sessions/containers/sessions-dashboard/sessions-dashboard.component.ts
+++ apps/joie/src/app/sessions/containers/sessions-dashboard/sessions-dashboard.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
-// import { CollectionReference, QueryFn } from '@angular/fire/firestore';
-// import { Session, Status } from '../../../../../../../libs/schemes/src';
 
 @Component({
   selector: 'app-sessions-dashboard',
   templateUrl: './sessions-dashboard.component.html',
   styleUrls: ['./sessions-dashboard.component.scss'],
 })
-export class SessionsDashboardComponent {
-  // queryFn(): QueryFn {
-  //   // validate key name
-  //   const orderBy: keyof Pick<Session, 'createdAt'> = 'createdAt';
-  //   const pageSize = 2;
-  //   return (ref: CollectionReference): firebase.firestore.Query<firebase.firestore.DocumentData> =>
-  //     ref.where('status', '==', Status.Public).orderBy(orderBy).limit(pageSize);
-  // }
-}
+export class SessionsDashboardComponent {}
