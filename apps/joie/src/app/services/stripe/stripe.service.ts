cb899f09cc937a8a2e5d789c3b87030755c6ff88
Ilir Hushi <ilirhushi@gmail.com>
2020-11-28 15:02:05 +0100

    #229 / minor refactored account-banking

--- apps/joie/src/app/services/stripe/stripe.service.ts
+++ apps/joie/src/app/services/stripe/stripe.service.ts
@@ -1,3 +1,4 @@
+import { IResponse } from '@joie/schemes';
 import { IStripe } from './../../../../../../libs/schemes/src/lib/models/stripe.model';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
@@ -22,5 +23,13 @@ export class StripeService {
   onboardCallback(accountID: string): Observable<any> {
     const callable = this.fns.httpsCallable('stripeOnboardCallback');
     return callable({ accountID });
+  }
+
+  async connectStripe(): Promise<void> {
+    const resp = (await this.onboard().toPromise()) as IResponse;
+
+    if (resp.type === 'success') {
+      window.open(resp.data.url, '_blank');
+    }
   }
 }
