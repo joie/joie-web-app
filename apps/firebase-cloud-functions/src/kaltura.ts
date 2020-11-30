f1a488f1ba992986051f01186a440d62fc1699e0
yinonov <yinon@hotmail.com>
2020-11-30 17:29:36 +0200

    alignment with docs

--- apps/firebase-cloud-functions/src/kaltura.ts
+++ apps/firebase-cloud-functions/src/kaltura.ts
@@ -1,20 +1,18 @@
 import { config, https } from 'firebase-functions';
+import { catchErrors } from './helpers';
 import { KalturaClient } from 'kaltura-typescript-client';
-import {
-  KalturaSessionType,
-  SessionStartAction,
-  KalturaLiveStreamEntry,
-  KalturaLiveStreamScheduleEvent,
-  KalturaLiveStreamScheduleEventArgs,
-  KalturaMediaType,
-  KalturaRecordStatus,
-  KalturaNullableBoolean,
-  KalturaDVRStatus,
-  KalturaScheduleEventRecurrenceType,
-  LiveStreamAddAction,
-  KalturaSourceType,
-} from 'kaltura-typescript-client/api/types';
-// import { catchErrors } from './helpers';
+import { KalturaSessionType } from 'kaltura-typescript-client/api/types/KalturaSessionType';
+import { SessionStartAction } from 'kaltura-typescript-client/api/types/SessionStartAction';
+import { KalturaLiveStreamEntry } from 'kaltura-typescript-client/api/types/KalturaLiveStreamEntry';
+import { KalturaLiveStreamScheduleEvent } from 'kaltura-typescript-client/api/types/KalturaLiveStreamScheduleEvent';
+import { KalturaLiveStreamScheduleEventArgs } from 'kaltura-typescript-client/api/types/KalturaLiveStreamScheduleEvent';
+import { KalturaMediaType } from 'kaltura-typescript-client/api/types/KalturaMediaType';
+import { KalturaRecordStatus } from 'kaltura-typescript-client/api/types/KalturaRecordStatus';
+import { KalturaNullableBoolean } from 'kaltura-typescript-client/api/types/KalturaNullableBoolean';
+import { KalturaDVRStatus } from 'kaltura-typescript-client/api/types/KalturaDVRStatus';
+import { KalturaScheduleEventRecurrenceType } from 'kaltura-typescript-client/api/types/KalturaScheduleEventRecurrenceType';
+import { LiveStreamAddAction } from 'kaltura-typescript-client/api/types/LiveStreamAddAction';
+import { KalturaSourceType } from 'kaltura-typescript-client/api/types/KalturaSourceType';
 
 // Kaltura TypeScript library, it uses XMLHttpRequest to connect to Kaltura API
 (global as any).XMLHttpRequest = require('xhr2');
@@ -37,7 +35,8 @@ const startSessionAndUpdateClient = async (): Promise<string | undefined> => {
       secret: admin_secret,
       partnerId: Number(partner_id),
       userId: user_secret,
-      type: KalturaSessionType.user, // what will be the difference? priviliges?
+      type: KalturaSessionType.admin, // what will be the difference? priviliges?
+      expiry: 86400, // TODO discuss preferred time
       // eventTags: 'vcprovider:newrow', // @ilirhushi clarify this
       // resourceTags: 'custom_rec_auto_start:1,custom_rs_show_participant:1', // @ilirhushi clarify this
       // targetId: 'kalturaVodPlayer', // @ilirhushi clarify this
@@ -59,7 +58,6 @@ const startSessionAndUpdateClient = async (): Promise<string | undefined> => {
 };
 
 const createLiveStreamEntry = async () => {
-  await startSessionAndUpdateClient();
   const liveStreamEntry = new KalturaLiveStreamEntry();
   liveStreamEntry.name = 'Test Live Entry With Cloud Transcoding';
   liveStreamEntry.description = 'Just a test live stream entry';
@@ -69,11 +67,11 @@ const createLiveStreamEntry = async () => {
   // LIVE_STREAM_FLASH designates a standard web live entry acquired via an RTMP or RTSP stream
   liveStreamEntry.mediaType = KalturaMediaType.liveStreamFlash;
   liveStreamEntry.recordStatus = KalturaRecordStatus.disabled;
-  // TODO await endSessionAndUpdateClient()
   return await client.request(new LiveStreamAddAction({ liveStreamEntry, sourceType: KalturaSourceType.liveStream }));
+  // TODO await endSessionAndUpdateClient()
 };
 
-export const createLiveStreamScheduleEvent = https.onCall(async (_, context) => {
+const createLiveStreamScheduleEvent = async () => {
   const data: KalturaLiveStreamScheduleEventArgs = {
     projectedAudience: 5,
     sourceEntryId: '',
@@ -81,22 +79,41 @@ export const createLiveStreamScheduleEvent = https.onCall(async (_, context) => 
     postEndTime: 0,
   };
   const scheduleEvent = new KalturaLiveStreamScheduleEvent(data);
-  scheduleEvent.summary = 'say a few words'; //the description of the event
-  // scheduleEvent.startDate = $startDate; //unix timestamp
-  // scheduleEvent.endDate = $endDate; //unix timestamp
+  scheduleEvent.summary = 'say a few words'; // the description of the event
+  scheduleEvent.startDate = new Date(); // unix timestamp
+  scheduleEvent.endDate = new Date(new Date().getTime() + 2 * 60 * 60 * 1000); // unix timestamp
   scheduleEvent.recurrenceType = KalturaScheduleEventRecurrenceType.none;
-});
+  return scheduleEvent;
+};
 
 export const addLiveStream = https.onCall(async (_, context) => {
-  return createLiveStreamEntry();
-});
+  await startSessionAndUpdateClient();
+  // first create the Live Entry
+  const liveEntry = await createLiveStreamEntry();
 
-// Define a Kaltura Entry as template entry for the event that includes the desired metadata for the recorded or live entry resulting from the scheduled event (including any information on how to publish the entry; co-editors, description/title, etc.).
-// Note: if you're scheduling a Live Event, set the Live Stream Entry id as the template of this Scheudled Event
-// $scheduleEvent->templateEntryId = $liveEntry->id;
-// // $client is the preinitilized KalturaClient instance
-// $schedulePlugin = KalturaScheduleClientPlugin::get($client);
-// $scheduleEvent = $schedulePlugin->scheduleEvent->add($scheduleEvent);
+  // Create the Scheduled Event:
+  // KalturaRecordScheduleEvent for a recording scheduling
+  // KalturaLiveStreamScheduleEvent for a live broadcast scheduling
+  // KalturaBlackoutScheduleEvent for scheduling a blackout date
+  const scheduleEvent = await createLiveStreamScheduleEvent();
+
+  // throw if no failed to create
+  if (!scheduleEvent || !liveEntry) {
+    throw new https.HttpsError('internal', 'failed to create Kaltura entry or event');
+  }
+
+  // Define a Kaltura Entry as template entry for the event that includes the desired
+  // metadata for the recorded or live entry resulting from the scheduled event
+  // (including any information on how to publish the entry; co - editors, description / title, etc.).
+  // Note: if you're scheduling a Live Event, set the Live Stream Entry id as the template of this Scheudled Event
+  scheduleEvent.templateEntryId = liveEntry?.id;
+
+  // $client is the preinitilized KalturaClient instance
+  // const schedulePlugin = KalturaScheduleClientPlugin.get(client);
+  // scheduleEvent = schedulePlugin.scheduleEvent.add($scheduleEvent);
+  // const schedulePlugin = KalturaScheduleClientPlugin::get($client)
+  return createLiveStreamScheduleEvent();
+});
 
 // // Create a resource to use with this event:
 // // KalturaCameraScheduleResource for a camera resource in a recorded event
